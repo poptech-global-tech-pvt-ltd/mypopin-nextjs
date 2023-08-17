4a330eba86da185e5dede4bc7833eb995f2d7e49
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-08-17 17:28:52 +0530

    fix : brand page ui fix

--- src/components/B2BCaseStudies/index.tsx
+++ src/components/B2BCaseStudies/index.tsx
@@ -110,7 +110,7 @@ function B2BCaseStudies() {
             <div style={{ width: "100%" }}>
                 <Carousel
                     arrows={false}
-                    autoPlay={true}
+                    autoPlay={false}
                     autoPlaySpeed={1800}
                     centerMode={false}
                     //   className=""
@@ -129,18 +129,37 @@ function B2BCaseStudies() {
                     responsive={responsive}
                     swipeable={true}
                 >
-                    <div>
-                        hello
-                    </div>
-                    <div>
-                        hello
-                    </div>
-                    <div>
-                        hello
-                    </div>
-                    <div>
-                        hello
-                    </div>
+                    <section>
+                        <div className="flex items-center justify-between max-w-7xl mx-auto px-4 lg:py-0">
+                            <div className={`text-[32px] lg:text-[70px] ${khand.className}`}>Case Studies</div>
+                            <div>
+                                <img className="w-[80px] h-[80px] lg:w-[170px] lg:h-[170px]" width="170" height="170" src="/casestudies/saaki-logo.svg" alt="anveshan logo" />
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 lg:flex max-w-6xl mx-auto justify-center items-center">
+                            <div className="mx-auto lg:m-0">
+                                <img className="lg:w-[371px] lg:h-[336px] w-[301px] h-[256px]" width="371" height="336" src="/casestudies/saaki.svg" alt="anveshan logo" />
+                            </div>
+                            <div className="py-2 lg:py-0" />
+                            <div className="px-2 lg:px-16">
+                                <div className={`text-[16px] lg:text-[22px] ${manrope.className} flex`}>
+                                    <div><img className="w-[70px] h-[70px] lg:w-[30px] lg:h-[30px]" src="/check.svg" /></div>
+                                    <div><span className="font-bold">16X more customers Signing</span> Up and transacting on the same day, in comparison to incumbents.</div>
+                                </div>
+                                <br />
+                                <div className={`text-[16px] lg:text-[22px] ${manrope.className} flex`}>
+                                    <div><img className="w-[70px] h-[70px] lg:w-[30px] lg:h-[30px]" src="/check.svg" /></div>
+                                    <div> <span className="font-bold">16X more customers Signing</span> Up and transacting on the same day, in comparison to incumbents.</div>
+                                </div>                                <br />
+                                <div className={`text-[16px] lg:text-[22px] ${manrope.className} flex`}>
+                                    <div><img className="w-[70px] h-[70px] lg:w-[30px] lg:h-[30px]" src="/check.svg" /></div>
+                                    <div> <span className="font-bold">16X more customers Signing</span> Up and transacting on the same day, in comparison to incumbents.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+
                 </Carousel>
             </div>
         </div>
