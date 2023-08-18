7d32690699a306c8fd2a78cf8cbeb7de226bf6ae
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-08-18 19:40:41 +0530

    feat : fetch allbrands data from strapi API

--- src/app/(allbrands)/allbrands/page.tsx
+++ src/app/(allbrands)/allbrands/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import Image from "next/image"
 import Link from "next/link"
 import { Manrope } from 'next/font/google'
+import { useEffect, useState } from "react"
 
 const manrope = Manrope({
     subsets: ['latin'],
@@ -8,6 +10,7 @@ const manrope = Manrope({
 })
 
 function AllBrands() {
+    const [allBrandsData, setAllBrandsData] = useState<any>();
 
     const allBrands = [
         { imageRelativeLink: "/allbrandsImages/almowear.png", alt: "brand logo", pathToUrl: "" },
@@ -42,23 +45,28 @@ function AllBrands() {
         { imageRelativeLink: "/allbrandsImages/zoh-offer.png", alt: "brand logo", pathToUrl: "" },
     ]
 
+    useEffect(() => {
+        fetch(`https://mypop-dashboard.popclub.co.in/api/brand-names?populate=*`).then((res) => res.json()).then((data) => setAllBrandsData(data?.data))
+    },[])
+
+   console.log({allBrandsData})
     return (
         <>
             <div className="pt-0 lg:pt-24">
                 <div className="py-6">
                     <div className={`text-center text-3xl ${manrope.className} font-bold py-10`}>Unmatched Offers & Discount Highlights</div>
                     <div className="grid grid-cols-1 lg:grid-cols-3 max-w-6xl mx-auto gap-14 justify-items-center">
-                        {allBrands?.map((i, index) => (
+                        {allBrandsData && allBrandsData?.map((i : any, index : number) => (
                             <div key={index} className="drop-shadow-xl transition-transform hover:scale-105">
-                                <Link href={i.pathToUrl}>
+                                {/* <Link href={i?.attributes?.brand_offer_card?.data?.attributes?.url}> */}
                                     <Image
-                                        src={i.imageRelativeLink}
+                                        src={i?.attributes?.brand_offer_card?.data?.attributes?.url}
                                         width={362}
                                         height={256}
                                         alt="facebook logo"
                                         className="cursor-pointer"
                                     />
-                                </Link>
+                                {/* </Link> */}
                             </div>
                         ))}
                     </div>
