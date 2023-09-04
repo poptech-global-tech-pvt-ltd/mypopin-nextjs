90ea6a09ca64973c6ccc18abb17efe8a0b179eed
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-09-04 12:48:46 +0530

    fix: add id to script

--- src/app/(IAmABrandPage)/partner-with-pop/layout.tsx
+++ src/app/(IAmABrandPage)/partner-with-pop/layout.tsx
@@ -22,7 +22,7 @@ export default function BrandLayout({ children }: BrandProps) {
                 <meta property="og:url" content="https://mypop.in/partner-with-pop" />
                 <link rel="canonical" href="https://mypop.in/partner-with-pop" />
                 {/* <!-- Facebook Pixel Code --> */}
-                <Script>
+                <Script id="fb-pixel-code">
                     {`
     !function(f,b,e,v,n,t,s)
     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -47,7 +47,7 @@ export default function BrandLayout({ children }: BrandProps) {
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest">
                     {/* <!-- Moengage Code --> */}
-                    <script type="text/javascript">
+                    <Script id="monage-code" type="text/javascript">
                         {`
     !function(e,n,i,t,a,r,o,d){var s=e[a]=e[a]||[];if(s.invoked=0,s.initialised>0||s.invoked>0)return console.error("MoEngage Web SDK initialised multiple times. Please integrate the Web SDK only once!"),!1;e.moengage_object=a;var l={},g=function n(i){return function(){for(var n=arguments.length,t=Array(n),a=0;a<n;a++)t[a]=arguments[a];(e.moengage_q=e.moengage_q||[]).push({f:i,a:t})}},u=["track_event","add_user_attribute","add_first_name","add_last_name","add_email","add_mobile","add_user_name","add_gender","add_birthday","destroy_session","add_unique_user_id","moe_events","call_web_push","track","location_type_attribute"],m={onsite:["getData","registerCallback"]};for(var c in u)l[u[c]]=g(u[c]);for(var v in m)for(var f in m[v])null==l[v]&&(l[v]={}),l[v][m[v][f]]=g(v+"."+m[v][f]);r=n.createElement(i),o=n.getElementsByTagName("head")[0],r.async=1,r.src=t,o.appendChild(r),e.moe=e.moe||function(){return(s.invoked=s.invoked+1,s.invoked>1)?(console.error("MoEngage Web SDK initialised multiple times. Please integrate the Web SDK only once!"),!1):(d=arguments.length<=0?void 0:arguments[0],l)},r.addEventListener("load",function(){if(d)return e[a]=e.moe(d),e[a].initialised=e[a].initialised+1||1,!0}),r.addEventListener("error",function(){return console.error("Moengage Web SDK loading failed."),!1})}(window,document,"script","https://cdn.moengage.com/webpush/moe_webSdk.min.latest.js","Moengage");
     Moengage = moe({
@@ -55,11 +55,11 @@ export default function BrandLayout({ children }: BrandProps) {
     debug_logs: 0
     });
     `}
-                    </script>
+                    </Script>
                     {/* <!-- End of Moengage Code --> */}
                 </link>
                 {/* <!-- Google tag (gtag.js) --> */}
-                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5HQ42CSKWF"></Script>
+                <Script id="google-tag-manager" async src="https://www.googletagmanager.com/gtag/js?id=G-5HQ42CSKWF"></Script>
                 <Script>
                     {`
 window.dataLayer = window.dataLayer || [];
