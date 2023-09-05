8accea67b53b2fe61f702d696efa35c278bc90ce
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-09-05 18:27:34 +0530

    fix: add suspense to brand page

--- src/app/(SpecificBrandPage)/brands/[slug]/page.tsx
+++ src/app/(SpecificBrandPage)/brands/[slug]/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import { BrandRewardsCarousel } from '@/components/BrandRewardsCarousel';
 import { BrandDeals } from '@/components/BrandDeals';
 import type { Metadata, ResolvingMetadata } from "next";
+import { Suspense } from 'react';
 
 
 const manrope = Manrope({
@@ -16,7 +17,6 @@ const manrope = Manrope({
 async function getData(brandName: string) {
     try {
         const res = await fetch(`https://mypop-dashboard.popclub.co.in/api/brand-names?filters[url][$eq]=${brandName}&populate=*`)
-
         if (!res.ok) {
             throw new Error('Failed to fetch data');
         }
@@ -34,107 +34,108 @@ async function BrandPage({ params }: any) {
 
     return (
         <>
-            <div className="pt-[3rem] lg:pt-[7.5rem] px-2">
-                <>
-                    <div className="test-container max-w-7xl mx-auto">
-                        <div style={{ backgroundColor: brandData?.primary_color }} className="itm1 p-6 rounded-3xl">
-                            <div className='flex justify-between'>
-                                <div>
-                                    <div style={{ color: brandData?.text_color }} className={`uppercase text-[28px] lg:text-5xl ${manrope.className} font-bold`}>EXTRA {brandData?.burn_callout?.split(" ")[1]}&nbsp;off with</div>
-                                    <div style={{ color: brandData?.text_color }} className={`uppercase text-5xl ${manrope.className} font-bold`}>
-                                        <div className='flex items-center'>
-                                            <div className='lg:text-5xl text-[28px] font-extrabold'>WITH</div>
-                                            <div className='px-2'><img width="35" height="35" src="/popcoin-icon.svg" /></div>
+            <Suspense fallback={<p>Loading Data...</p>}>
+                <div className="pt-[3rem] lg:pt-[7.5rem] px-2">
+                    <>
+                        <div className="test-container max-w-7xl mx-auto">
+                            <div style={{ backgroundColor: brandData?.primary_color }} className="itm1 p-6 rounded-3xl">
+                                <div className='flex justify-between'>
+                                    <div>
+                                        <div style={{ color: brandData?.text_color }} className={`uppercase text-[28px] lg:text-5xl ${manrope.className} font-bold`}>EXTRA {brandData?.burn_callout?.split(" ")[1]}&nbsp;off with</div>
+                                        <div style={{ color: brandData?.text_color }} className={`uppercase text-5xl ${manrope.className} font-bold`}>
+                                            <div className='flex items-center'>
+                                                <div className='lg:text-5xl text-[28px] font-extrabold'>WITH</div>
+                                                <div className='px-2'><img width="35" height="35" src="/popcoin-icon.svg" /></div>
+                                            </div>
                                         </div>
+                                        <div style={{ color: brandData?.text_color }} className={`lg:text-xl text-[17px] ${manrope.className}`}>{brandData?.sub_title}</div>
                                     </div>
-                                    <div style={{ color: brandData?.text_color }} className={`lg:text-xl text-[17px] ${manrope.className}`}>{brandData?.sub_title}</div>
+                                    <div>
+                                        <div style={{ backgroundColor: brandData?.primary_color, color: brandData?.text_color }} className={`h-[100px] w-[100px] lg:h-[200px] lg:w-[200px] rounded-full flex justify-center items-center translate-x-6 -translate-y-16 lg:translate-x-13 lg:-translate-y-45 ${manrope.className} text-[15px] lg:text-2xl font-bold text-center p-8 text-white pulsating-border`}>Limited Time Deal</div>
+                                    </div>
                                 </div>
-                                <div>
-                                    <div style={{ backgroundColor: brandData?.primary_color, color: brandData?.text_color }} className={`h-[100px] w-[100px] lg:h-[200px] lg:w-[200px] rounded-full flex justify-center items-center translate-x-6 -translate-y-16 lg:translate-x-13 lg:-translate-y-45 ${manrope.className} text-[15px] lg:text-2xl font-bold text-center p-8 text-white pulsating-border`}>Limited Time Deal</div>
+                                <div className='flex justify-between items-center'>
+                                    <a href={brandData?.redirection_url}>
+                                        <Button className={`${manrope.className} bg-white text-lg text-black hover:text-white font-bold rounded-full`}>Shop with POPCoins</Button>
+                                    </a>
+                                    <div className='hidden lg:block'>
+                                        <img src={brandData?.round_logo?.data?.attributes?.url} />
+                                    </div>
                                 </div>
                             </div>
-                            <div className='flex justify-between items-center'>
-                                <a href={brandData?.redirection_url}>
-                                    <Button className={`${manrope.className} bg-white text-lg text-black hover:text-white font-bold rounded-full`}>Shop with POPCoins</Button>
-                                </a>
-                                <div className='hidden lg:block'>
-                                    <img src={brandData?.round_logo?.data?.attributes?.url} />
-                                </div>
+                            <div className="itm2">
+                                <Image
+                                    src={brandData?.brand_hero_image_small_1?.data?.attributes?.url}
+                                    width="0"
+                                    height="0"
+                                    sizes="100vw"
+                                    className="w-full h-full rounded-lg lg:rounded-3xl object-cover aspect-square  md:aspect-auto lg:aspect-auto"
+                                    alt=""
+                                    style={{ objectFit: "cover" }}
+                                />
+                            </div>
+                            <div className="itm3 ">
+                                <Image
+                                    src={brandData?.brand_hero_image_large?.data?.attributes?.url}
+                                    width="0"
+                                    height="0"
+                                    sizes="100vw"
+                                    className="w-full h-full rounded-lg lg:rounded-3xl object-cover aspect-square md:aspect-auto lg:aspect-auto"
+                                    alt=""
+                                    style={{ objectFit: "cover" }}
+                                />
+                            </div>
+                            <div className="itm4">
+                                <Image
+                                    src={brandData?.brand_hero_image_small_2?.data?.attributes?.url}
+                                    width="0"
+                                    height="0"
+                                    sizes="100vw"
+                                    className="w-full h-full rounded-lg lg:rounded-3xl object-cover aspect-square md:aspect-auto lg:aspect-auto"
+                                    alt=""
+                                />
                             </div>
                         </div>
-                        <div className="itm2">
-                            <Image
-                                src={brandData?.brand_hero_image_small_1?.data?.attributes?.url}
-                                width="0"
-                                height="0"
-                                sizes="100vw"
-                                className="w-full h-full rounded-lg lg:rounded-3xl object-cover aspect-square  md:aspect-auto lg:aspect-auto"
-                                alt=""
-                                style={{ objectFit: "cover" }}
-                            />
-                        </div>
-                        <div className="itm3 ">
-                            <Image
-                                src={brandData?.brand_hero_image_large?.data?.attributes?.url}
-                                width="0"
-                                height="0"
-                                sizes="100vw"
-                                className="w-full h-full rounded-lg lg:rounded-3xl object-cover aspect-square md:aspect-auto lg:aspect-auto"
-                                alt=""
-                                style={{ objectFit: "cover" }}
-                            />
-                        </div>
-                        <div className="itm4">
-                            <Image
-                                src={brandData?.brand_hero_image_small_2?.data?.attributes?.url}
-                                width="0"
-                                height="0"
-                                sizes="100vw"
-                                className="w-full h-full rounded-lg lg:rounded-3xl object-cover aspect-square md:aspect-auto lg:aspect-auto"
-                                alt=""
-                            />
-                        </div>
-                    </div>
-                </>
+                    </>
 
-                {/* // about section */}
-                <br />
-                <br />
-                <hr className='max-w-7xl mx-auto' />
-                <br />
-                <div className='max-w-7xl mx-auto'>
-                    <div className={`font-bold ${manrope.className} text-[22px] font-bold text-center lg:text-left `}>About {brandData?.brand_name}</div>
+                    {/* // about section */}
                     <br />
-                    <div className={`text-center lg:text-left ${manrope.className} font-[500] text-[16px]`}>
-                        {brandData?.about_brand}
-                    </div>
-                </div>
-                <br />
-                <br />
-                <hr className='max-w-7xl mx-auto' />
-                <BrandRewardsCarousel />
-                <BrandDeals discountPercentage={brandData?.discount_percentage} primaryColor={brandData?.primary_color} secondaryColor={brandData?.secondary_color} textColor={brandData?.text_color} />
-                <hr className='max-w-7xl mx-auto' />
-                <section className='max-w-7xl mx-auto'>
-                    <div className={`${manrope.className} text-center text-[22px] lg:text-3xl font-bold py-8`}>
-                        <div className='flex items-center justify-center'>
-                            <div>Earn&nbsp;</div>
-                            <div><img src="/popcoin-icon.svg" /></div>
-                            <div>&nbsp;5&nbsp;</div>
-                            <div>For&nbsp;</div>
-                            <div>Every&nbsp;</div>
-                            <div>₹100&nbsp;</div>
+                    <br />
+                    <hr className='max-w-7xl mx-auto' />
+                    <br />
+                    <div className='max-w-7xl mx-auto'>
+                        <div className={`font-bold ${manrope.className} text-[22px] font-bold text-center lg:text-left `}>About {brandData?.brand_name}</div>
+                        <br />
+                        <div className={`text-center lg:text-left ${manrope.className} font-[500] text-[16px]`}>
+                            {brandData?.about_brand}
                         </div>
                     </div>
-                    <div className={`${manrope.className} text-center text-[16px] lg:text-2xl font-normal pb-8`}>Keep earning POPcoins with every purchase from  {brandData?.brand_name} and other brands on POPcoins</div>
-                    <div className='text-center'>
-                        <a href={brandData?.redirection_url}>
-                            {/* <Button style={{ backgroundColor: brandData?.text_color }} onClick={() => handleBrandBtnClick(brandData)} className={`rounded-full ${manrope.className} text-xl p-6 drop-shadow-md`}>Shop {brandData?.brand_name} Now</Button> */}
-                        </a>
-                    </div>
-                </section>
-            </div>
-
+                    <br />
+                    <br />
+                    <hr className='max-w-7xl mx-auto' />
+                    <BrandRewardsCarousel />
+                    <BrandDeals discountPercentage={brandData?.discount_percentage} primaryColor={brandData?.primary_color} secondaryColor={brandData?.secondary_color} textColor={brandData?.text_color} />
+                    <hr className='max-w-7xl mx-auto' />
+                    <section className='max-w-7xl mx-auto'>
+                        <div className={`${manrope.className} text-center text-[22px] lg:text-3xl font-bold py-8`}>
+                            <div className='flex items-center justify-center'>
+                                <div>Earn&nbsp;</div>
+                                <div><img src="/popcoin-icon.svg" /></div>
+                                <div>&nbsp;5&nbsp;</div>
+                                <div>For&nbsp;</div>
+                                <div>Every&nbsp;</div>
+                                <div>₹100&nbsp;</div>
+                            </div>
+                        </div>
+                        <div className={`${manrope.className} text-center text-[16px] lg:text-2xl font-normal pb-8`}>Keep earning POPcoins with every purchase from  {brandData?.brand_name} and other brands on POPcoins</div>
+                        <div className='text-center'>
+                            <a href={brandData?.redirection_url}>
+                                {/* <Button style={{ backgroundColor: brandData?.text_color }} onClick={() => handleBrandBtnClick(brandData)} className={`rounded-full ${manrope.className} text-xl p-6 drop-shadow-md`}>Shop {brandData?.brand_name} Now</Button> */}
+                            </a>
+                        </div>
+                    </section>
+                </div>
+            </Suspense>
         </>
     )
 }
@@ -146,11 +147,11 @@ export async function generateMetadata(
     { params, searchParams }: any,
     parent: ResolvingMetadata
 ): Promise<Metadata> {
-   
+
     const brandName = params.slug
     const res = await getData(brandName)
     const brandData = res?.data[0]?.attributes
-    
+
     return {
         title: `${brandData?.brand_name} X POPcoins: Get Discounts & Rewards on Body Tales using POPcoins | POP`,
         description: `Shop your Favorite Brand ${brandData?.brand_name} using POP to Earn Additional Rewards, Discounts and POPcoins. Click here to learn more about POPcoins X Body Tales`,
