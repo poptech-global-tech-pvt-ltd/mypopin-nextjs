0525d2cad59a2687a733a1f351cb306df9767826
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-09-07 15:10:28 +0530

    fix: missing key in infinite slider

--- src/components/InfinteImageSlider/index.tsx
+++ src/components/InfinteImageSlider/index.tsx
@@ -17,7 +17,7 @@ function InfiniteImageSlider() {
                     <div>
                         <ul>
                             {brandLogos?.map((i: any, index: number) => (
-                                <li>
+                                <li key={index}>
                                     <Link target="_blank" href={`/brands/${i?.attributes?.redirection_url}`}>
                                         <img className="min-w-[100px] lg:min-w-[200px] h-auto" src={i?.attributes?.image?.data?.attributes?.url} />
                                     </Link>
