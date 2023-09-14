aa7de93ca5a6a2b8c12c19a1121fb87ee8479bfb
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-09-14 14:25:22 +0530

    fix: integration section design and logo add

--- src/components/B2BIntegrations/index.tsx
+++ src/components/B2BIntegrations/index.tsx
@@ -1,9 +1,6 @@
 'use client'
-
 import { Khand } from 'next/font/google'
 import { Manrope } from 'next/font/google'
-import Image from 'next/image'
-import { Button } from '../ui/button'
 import Link from 'next/link'
 
 const khand = Khand({
@@ -25,21 +22,35 @@ const integrationLogos = [
     { url: "/integration-shiprocket.png" },
     { url: "/integration-shopflo.png" },
     { url: "/integration-simpl.png" },
-    // { url: "/integration-wigzo.png" },
-    
+    { url: "/moengage-logo-white.png" },
+    { url: "/webengage-logo-white.png" },
 ]
 
 function B2BIntegrations() {
     return (
+        // <div className='flex'>
+        //     {integrationLogos?.map((i, index) => (
+        //         <div className='bg-red-300' key={index}>
+        //             <img className='' width="157" height="auto" src={i.url} alt={i.url} />
+        //         </div>
+        //     ))}
+        // </div>
         <div className="px-12 py-4 lg:p-16 integrations-section">
             <div className="max-w-6xl mx-auto">
                 <Link href="/integrations">
                     <div className={`${khand.className}  text-5xl lg:text-[70px] text-center py-8 text-white`}>Integrations</div>
                     <div className="container mx-auto p-4">
-                        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 items-center justify-items-center">
+                        {/* <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 items-center justify-items-center justify-self-center">
                             {integrationLogos?.map((i, index) => (
                                 <div key={index}>
                                     <img className='lg:w-[157px] lg:h-auto w-[80px] h-auto' width="auto" height="157" src={i.url} alt={i.url} />
+                                </div>
+                            ))}
+                        </div> */}
+                        <div className='flex flex-wrap items-center justify-center'>
+                            {integrationLogos?.map((i, index) => (
+                                <div key={index}>
+                                    <img className='lg:w-[157px] lg:h-auto w-[80px] h-auto mx-2 my-4' src={i.url} alt={i.url} />
                                 </div>
                             ))}
                         </div>
