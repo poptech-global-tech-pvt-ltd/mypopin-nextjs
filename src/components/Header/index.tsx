ba736351642e7cf2ef02ed30f1f554a0c95941bc
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-09-15 18:03:05 +0530

    fix: active tab on blogs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -217,7 +217,7 @@ function Header() {
                             )}
                             <Tabs value={pathName} className="navbar-container" defaultValue="customer">
                                 <TabsList className="tabslist h-[45px] p-2">
-                                    <TabsTrigger data-state={(pathName.split('/')[1] === "brands") || (pathName === "/") || (pathName.split('/')[1] === "coupons") ? "active" : "inactive" } className="tabstrigger h-[35px]" value="/"><Link href="/">I am a Customer</Link></TabsTrigger>
+                                    <TabsTrigger data-state={(pathName.split('/')[1] === "brands") || (pathName === "/allbrands") || (pathName === "/") || (pathName === "/blogs") || (pathName.split('/')[1] === "blogs") || (pathName.split('/')[1] === "coupons") ? "active" : "inactive" } className="tabstrigger h-[35px]" value="/"><Link href="/">I am a Customer</Link></TabsTrigger>
                                     <TabsTrigger data-state={(pathName === '/features') || (pathName === "/partner-with-pop") || (pathName === "/integrations") || (pathName === "/features") ? "active" : "inactive"} className="tabstrigger h-[35px]" value="/partner-with-pop"> <Link href="/partner-with-pop">I am a Brand</Link></TabsTrigger>
                                 </TabsList>
                             </Tabs>
