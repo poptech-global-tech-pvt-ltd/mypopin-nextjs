614fcf11b89525cbebdb8ae8a45be04f458f56e7
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-09-15 13:00:00 +0530

    fix: numbers count

--- src/components/POPInNumbers/index.tsx
+++ src/components/POPInNumbers/index.tsx
@@ -23,8 +23,8 @@ const popInNumbers = [
 
 function POPInNumbers() {
     const [count, setCount] = useState(1);
-    const [custCount, setCustCount] = useState(915556)
-    const [savingsCount, setSavingCount] = useState(2042508);
+    const [custCount, setCustCount] = useState(920500)
+    const [savingsCount, setSavingCount] = useState(2061024);
     const noOfBrandRef = useRef(null);
     const noOfCustomers = useRef(null);
     const noOfSaving = useRef(null)
