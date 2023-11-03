712628b5a5dd05d9c40aafda64637a13ba62122b
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-11-03 13:04:23 +0530

    fix: conditional header and footer for ref=app link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,22 @@ import { MobileHeader } from '@/components/MobileHeader'
 import { Footer } from '@/components/Footer'
 import Transition from '@/components/Transition/Transition'
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [searchParam, setSearchParam] = useState<string>();
   const pathname = usePathname()
+  console.log("-->", window.location.search)
+
+  useEffect(() => {
+    if (window.location.search != null) {
+      setSearchParam(window.location.search)
+    }
+  }, [])
+
+  console.log({ searchParam })
+  console.log("check-", pathname !== '/coupons' || searchParam !== "?ref=app")
+
   return (
     <html lang="en">
       <head>
@@ -16,13 +29,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Transition>
           <div className="hidden lg:block">
-            <Header />
+            {searchParam !== "?ref=app" ? <Header /> : null}
           </div>
           <div className="block lg:hidden">
             <MobileHeader />
           </div>
           {children}
-          {pathname !== '/coupons' ? <Footer /> : null}
+          {(pathname !== '/coupons' && searchParam !== "?ref=app") ? <Footer /> : null}
         </Transition>
       </body>
     </html>
